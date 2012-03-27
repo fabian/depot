9ef678b03e04ccf5a527fdc77fbcd1ea206f9e5b
Fabian Vogler <fabian@equivalence.ch>
2012-03-27 10:43:41 +0200

    Validate product length

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, length: {minimum: 10}
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)$}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
