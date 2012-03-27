323f821813a37af6f564575e055fc1296ff0142a
Fabian Vogler <fabian@equivalence.ch>
2012-03-27 10:58:28 +0200

    Override field to remove validation message

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Override field to remove validation message
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+  "#{html_tag}".html_safe
+end
+
 # Initialize the rails application
 Depot::Application.initialize!
