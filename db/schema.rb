581dc9b78c96427cfff808b437c3c1e42b3c4242
Fabian Vogler <fabian@equivalence.ch>
2012-05-28 23:25:09 +0200

    Migration for old carts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120528211750) do
+ActiveRecord::Schema.define(:version => 20120528212143) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
